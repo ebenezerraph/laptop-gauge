596d7ad6e9fa8ee930100a7427a9d826b74d4ef6
Ebenezer Raphael <ebenezerraphael0@gmail.com>
2024-06-14 17:00:03 +0100

    Final test

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const messageDiv = document.getElementById('message');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-  fetch('http://localhost:8080/data')
+  fetch('https://serious-person-tropical-balance-beta.pipeops.app/data')
     .then(response => response.json())
     .then(data => {
       messageDiv.textContent = data.message;
